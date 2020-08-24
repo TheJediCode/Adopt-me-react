388ac0555a4d27d0dae9888061f8baa272849831
TheJediCode <68616265+TheJediCode@users.noreply.github.com>
2020-08-24 19:52:27 -0400

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,30 @@
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt me!"),
+    React.createElement(Pet, {
+      name: "Link",
+      animal: "Dog",
+      breed: "Hylian",
+    }),
+    React.createElement(Pet, {
+      name: "Zelda",
+      animal: "Cat",
+      breed: "Royal Hylian",
+    }),
+    React.createElement(Pet, {
+      name: "Navi",
+      animal: "Fairy",
+      breed: "Fairy-Fly",
+    }),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
