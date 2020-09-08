b6fca8dcdf5eb22fdd0cfc6e017d976b4a470438
TheJediCode <68616265+TheJediCode@users.noreply.github.com>
2020-09-07 20:50:02 -0400

    Second commit

--- src/app.js
+++ src/app.js
@@ -1,30 +1,15 @@
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-  ]);
-};
+import React from 'react';
+import { render } from 'react-dom';
+//import Pet from './Pet';
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt me!"),
-    React.createElement(Pet, {
-      name: "Link",
-      animal: "Dog",
-      breed: "Hylian",
-    }),
-    React.createElement(Pet, {
-      name: "Zelda",
-      animal: "Cat",
-      breed: "Royal Hylian",
-    }),
-    React.createElement(Pet, {
-      name: "Navi",
-      animal: "Fairy",
-      breed: "Fairy-Fly",
-    }),
-  ]);
+  return (
+    <div>
+      <h1 id="something-important">Adopt me!</h1>
+      <SearchParams />
+    </div>
+  )
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(<App/>, document.getElementById("root"));
